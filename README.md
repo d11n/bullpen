dd8af5fa90fd8895718701b1d49bba8ad1bea35f
Derek Dunagan <derek.dunagan@gmail.com>
2018-03-16 19:40:41 -0600

    add stability warning

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bullpen
 JavaScript store interface for simplifying data flow and state management, works well with Redux and Mobx
+
+☠️ BULLPEN is under active development and will change often. Not until it reaches version 0.1.0 will its interface be relatively stable. ☠️
--- source/index.js
+++ source/index.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line max-params
 (function main(API, Collection) {
     const { Mofo_api, Rest_api } = API;
+    console.warn([
+        '☠️  BULLPEN is under active development and will change often.',
+        'Not until it reaches version 0.1.0',
+        'will its interface be relatively stable. ☠️',
+        ].join(' ')); // eslint-disable-line indent
     return module.exports = Object.freeze({
         Mofo_api,
         Rest_api,
